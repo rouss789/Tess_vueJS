eaa15425abfe211dca87fd50c118c1d23f84cc87
Rosa Rojas <rojaso.rosa@gmail.com>
2024-07-28 21:38:34 -0600

    se crea estructura de proyecto

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <h1>votaciones de productos</h1>
+  <div>
+    <div>
+      <div>
+        <h2></h2>
+          <button></button>
+        <p></p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+</script>
+
+<style>
+
+</style>
