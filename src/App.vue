831d4c90f291f29da9c66e4059b7b5ceeeb3e560
Rosa Rojas <rojaso.rosa@gmail.com>
2024-07-28 22:41:31 -0600

    se agrega data, methods

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,11 @@
 <template>
   <h1>votaciones de productos</h1>
-  <div>
-    <div>
-      <div>
-        <h2></h2>
-          <button></button>
+  <div id="app">
+    <div class="contenedor">
+      <div v-for="(producto, index)" :key="index" class="tarjeta_producto" :style="{backgroundColor: producto.color}">
+        <h2>{{ producto.nombre }}</h2>
+        <p>Puntos:{{ producto.votos }}</p>
+          <button @click="votar(idex)">Votar</button>
         <p></p>
       </div>
     </div>
@@ -12,6 +13,23 @@
 </template>
 
 <script>
+export default{
+  name: 'App',
+  data(){
+    return{ 
+      productos:[
+        {nombre:'Producto Azul', color:'blue', votos:0},
+        {nombre:'Producto Rojo', color:'red', votos:0},
+        {nombre:'Producto Morado',color:'purple', votos:0}
+      ]
+    };
+  },
+  methods:{
+    votar(idex){
+      this.productos[index].votos +=1;
+    }
+  }
+};
 </script>
 
 <style>
